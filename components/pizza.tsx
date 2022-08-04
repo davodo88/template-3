8ce3236d6e74e9d1dcdc4fffb41f85393cc5fdff
davodo88 <92156631+davodo88@users.noreply.github.com>
2022-08-04 11:09:29 +0200

    slide function

--- components/pizza.tsx
+++ components/pizza.tsx
@@ -33,33 +33,41 @@ import Image from 'next/image';
   
 
     return (
-      <div id="pizzas" className="flex flex-row py-24 w-screen h-auto ">
+      <div id="pizzas" className="flex flex-row py-20 w-screen h-auto ">
         <button
-         onClick={()=>counter > 0 ? setCounter(counter - 1) : null }
-          className={classNames(
-            "w-12 h-12 m-auto bg-gray-100 rounded-full"
-          )}
+          onClick={() => (counter > 0 ? setCounter(counter - 1) : null)}
+          className={classNames("w-12 h-12 m-auto bg-gray-100 rounded-full")}
         >
           <FontAwesomeIcon icon={faArrowLeft} />
         </button>
-            <div
-              className={classNames("flex flex-col my-24 border-2 mx-auto w-72 h-72 rounded-md pt-3")}
-            >
-              <span
-                className={classNames(
-                  "flex flex-col text-center text-xl mt-2 font-medium text-white"
-                )}
-              >
-             
-              </span>
-              <span className="flex justify-center hover:underline decoration-white text-white my-4">
-                {muestras[counter].text}
-              </span>
-              
-            </div>
-        
+        <div
+          className={classNames(
+            "flex flex-col my-44 mx-auto  rounded-md p-10"
+          )}>
+          <span
+            className={classNames(
+              "flex flex-col text-center text-2xl my-2 p-4 font-bold font-Pro text-white"
+            )}>
+            {muestras[counter].title}
+          </span>
+          <span className="flex justify-center font-Pro text-white my-4">
+            {muestras[counter].text}
+          </span>
+          <div className="w-44 h-44 m-auto">
+            <Image
+              src={muestras[counter].src}
+              width={400}
+              height={400}
+              alt={muestras[counter].title}
+              className="rounded-full m-2"
+            />
+          </div>
+        </div>
+
         <button
-          onClick={()=>counter < samples - 1 ? setCounter(counter + 1) : null }
+          onClick={() =>
+            counter < samples - 1 ? setCounter(counter + 1) : null
+          }
           className="w-12 h-12 m-auto bg-gray-100 rounded-full"
         >
           <FontAwesomeIcon icon={faArrowRight} />
